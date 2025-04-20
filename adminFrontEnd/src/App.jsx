3c5bd09b2fca8374e32c415affe3cb66857a0131
Ayush Pratap <ayushprince110@gmail.com>
2025-04-20 21:34:07 +0530

    completed adminFrontEnd

--- adminFrontEnd/src/App.jsx
+++ adminFrontEnd/src/App.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
-import {Routes,Route} from 'react-router-dom'
 import Add from './screens/Add/Add'
-import Orders from './screens/Orders/Orders'
 import List from './screens/List/List'
-
+import Orders from './screens/Orders/Orders'
+import { Routes, Route } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import './App.css'
 const App = () => {
+  // const url = "http://localhost:4000"
+  const url = "https://foodprep-uezu.onrender.com"
   return (
     <div className='app'>
-      <Navbar/>
+      <ToastContainer />
+      <Navbar />
       <hr />
-      <div className="app-content">
-        <Sidebar/>
+      <div className='app-content'>
+        <Sidebar />
         <Routes>
-          <Route path ="/" element={<Add/>}></Route>
-          <Route path ="/add" element={<Add/>}></Route>
-          <Route path='/list' element={<List/>}></Route>
-          <Route path='/orders' element={<Orders/>}></Route>
+          <Route path='/' element={<Add url={url} />}></Route>
+          <Route path='/add' element={<Add url={url} />}></Route>
+          <Route path='/list' element={<List url={url} />}></Route>
+          <Route path='/orders' element={<Orders url={url} />}></Route>
+
         </Routes>
+
       </div>
     </div>
   )
--- adminFrontEnd/src/components/Sidebar/Sidebar.jsx
+++ adminFrontEnd/src/components/Sidebar/Sidebar.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import './Sidebar.css'
-import {assets} from '../../assets/assets'
-import {NavLink} from 'react-router-dom'
+import { assets } from '../../assets/assets'
+import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       <div className="sidebar-options">
         <NavLink to='/add' className="sidebar-option">
-          <img src={assets.add_icon} alt="" />
+          <img src={assets.add_icon} className='add-icon' />
           <p>Add Items</p>
         </NavLink>
-        <NavLink  to='/list' className="sidebar-option">
-          <img src={assets.list_icon} alt="" />
+        <NavLink to='/list' className="sidebar-option ">
+          <img src={assets.list_icon} className='list-icon' />
           <p>List Items</p>
         </NavLink>
         <NavLink to='/orders' className="sidebar-option">
-          <img src={assets.order_icon} alt="" />
+          <img src={assets.order_icon} className='order-icon' />
           <p>Orders</p>
         </NavLink>
       </div>
